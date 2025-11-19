8006b4a26c0c7046ef4ca8e1057ad62eb5b96add
Hong Jiarong <me@jrhim.com>
2025-11-19 22:35:16 +0800

    ci, cross platform

--- xmake.lua
+++ xmake.lua
@@ -14,6 +14,23 @@ local function add_protocol_target(name, source)
         add_files(source)
         if is_plat("macosx") then
             add_shflags("-undefined dynamic_lookup", { force = true })
+        elseif is_plat("linux") then
+            -- Allow undefined symbols to be resolved at runtime by the Rust host
+            add_shflags("-Wl,--unresolved-symbols=ignore-all", { force = true })
+        elseif is_plat("windows") then
+            -- Windows: Allow unresolved external symbols (imported from Rust host at runtime)
+            -- We export our own symbols via __declspec(dllexport) in Export.hpp
+            if is_toolchain("msvc") then
+                -- MSVC linker
+                add_ldflags("/FORCE:UNRESOLVED", { force = true })
+            else
+                -- MinGW/GCC linker
+                add_shflags("-Wl,--unresolved-symbols=ignore-all", { force = true })
+            end
+            if is_mode("debug") then
+                set_optimize("none")
+                set_symbols("debug")
+            end
         end
     target_end()
 end
